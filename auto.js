d4674df051a0b5a1e81505f83fc9277cd7a5135d
shahpratibha <pratibhashah1995@gmail.com>
2024-03-26 12:12:25 +0530

    merge+

--- auto.js
+++ auto.js
@@ -19,8 +19,6 @@ var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     //   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-  
-
 var Esri_WorldImagery = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     {
@@ -33,99 +31,27 @@ var baseLayers = {};
 
 
 
-// var Revenue_Layer = L.tileLayer
-//     .wms("https://portal.geopulsea.com/geoserver/pmc/wms", {
-//         layers: "Revenue",
-//         format: "image/png",
-//         transparent: true,
-//         tiled: true,
-//         version: "1.1.0",
-//         // attribution: "Revenue",
-//         opacity: 1,
-//     }).addTo(map);
+var Revenue_Layer = L.tileLayer
+    .wms("https://portal.geopulsea.com/geoserver/pmc/wms", {
+        layers: "Revenue",
+        format: "image/png",
+        transparent: true,
+        tiled: true,
+        version: "1.1.0",
+        // attribution: "Revenue",
+        opacity: 1,
+    }).addTo(map);
 
 
-// var WMSlayers = {
-//   "OSM": osm,
-//   "Esri": Esri_WorldImagery,
-//   "Satellite": googleSat,
-//   Revenue: Revenue_Layer,
- 
-// };
-var wms_layer1 = L.tileLayer.wms(
-  "https://portal.geopulsea.com/geoserver/AutoDCR/wms",
-  {
-    layers: "PLU_Ward",
-    format: "image/png",
-    transparent: true,
-    tiled: true,
-    version: "1.1.0",
-    // attribution: "Exist_Road",
-    opacity: 1,
-  }).addTo(map);
- 
-var wms_layer16 = L.tileLayer.wms(
-  "https://portal.geopulsea.com/geoserver/AutoDCR/wms",
-  {
-    layers: "Ward_Boundary",
-    format: "image/png",
-    transparent: true,
-    tiled: true,
-    version: "1.1.0",
-    // attribution: "Exist_Road",
-    opacity: 1,
-  }
-);
- 
-var wms_layer12 = L.tileLayer
-  .wms("https://portal.geopulsea.com/geoserver/AutoDCR/wms", {
-    layers: "DP_Ward_Road",
-    format: "image/png",
-    transparent: true,
-    tiled: true,
-    version: "1.1.0",
-    // attribution: "DP_Roads",
-    opacity: 1,
-    maxZoom: 25,
-  }).addTo(map);
- 
-var wms_layer11 = L.tileLayer
-  .wms("https://portal.geopulsea.com/geoserver/AutoDCR/wms", {
-    layers: "Revenue",
-    format: "image/png",
-    transparent: true,
-    tiled: true,
-    version: "1.1.0",
-    // attribution: "Reservations",
-    opacity: 1,
-  });
- 
-var wms_layer13 = L.tileLayer.wms(
-  "https://portal.geopulsea.com/geoserver/AutoDCR/wms",
-  {
-    layers: "PMC_Boundary",
-    format: "image/png",
-    transparent: true,
-    tiled: true,
-    version: "1.1.0",
-    // attribution: "Drainage_data",
-    opacity: 1,
-  }
-);
- 
- 
-// //////////////////////////added 11-03-2023/////////////////////////////////////////
- 
 var WMSlayers = {
-  "OSM": osm,
-  "Esri": Esri_WorldImagery,
-  "Satellite": googleSat,
-  PLU_Ward: wms_layer1,
-  Ward: wms_layer16,
-  DP_roads: wms_layer12,
-  Revenue: wms_layer11,
-  Boundary: wms_layer13,
+    "OSM": osm,
+    "Esri": Esri_WorldImagery,
+    "Satellite": googleSat,
+
+    Revenue: Revenue_Layer,
+
 };
+
 
 var control = new L.control.layers(baseLayers, WMSlayers).addTo(map);
 control.setPosition('topright');
@@ -211,111 +137,6 @@ $("#search_type").change(function () {
 
     // Update Revenue_Layer with new CQL_FILTER
 
-// FitbouCustomiseRevenue(filters)
-// Revenue_Layer.setParams({
-//     CQL_FILTER: filters,
-//     maxZoom: 19.5,
-//     styles: "polygon"
-// });
-
-
-// function getvalues(callback) {
-//     var geoServerURL =
-//         "https://portal.geopulsea.com//geoserver/pmc/wms?service=WFS&version=1.1.0&request=GetFeature&typeName=Revenue&propertyName=Gut_No&outputFormat=application/json";
-
-//     if (filters) {
-//         geoServerURL += "&CQL_FILTER=" + encodeURIComponent(filters);
-//     }
-
-//     $.getJSON(geoServerURL, function (data) {
-//         var gutvalues = new Set();
-
-//         // Populate the Set with gut numbers
-//         $.each(data.features, function (index, feature) {
-//             var gutss = feature.properties.Gut_No;
-//             gutvalues.add(gutss);
-//         });
-
-//         // Convert the Set to an array
-//         var Uniqueguts = Array.from(gutvalues);
-//         console.log(Uniqueguts, "Uniqueguts");
-
-//         // Call the callback function if it's provided
-//         if (callback && typeof callback === "function") {
-//             callback(Uniqueguts);
-//         }
-//     });
-// }
-
-// // Call getvalues function and pass a callback function to handle Uniqueguts
-// getvalues(function (Uniqueguts) {
-//     console.log(Uniqueguts, "Uniqueguts");
-
-//     // Add checkboxes for each Uniqueguts value
-//     var container = document.getElementById("checkboxContainer");
-//     container.innerHTML = ""; // Clear previous checkboxes
-
-//     Uniqueguts.forEach(function (value) {
-//         var checkbox = document.createElement("input");
-//         checkbox.type = "checkbox";
-//         checkbox.id = "checkbox_" + value.replace(/\s+/g, "_"); // Use a unique ID for each checkbox
-//         checkbox.value = value;
-
-//         var label = document.createElement("label");
-//         label.htmlFor = checkbox.id;
-//         label.textContent = value;
-
-//         container.appendChild(checkbox);
-//         container.appendChild(label);
-//         container.appendChild(document.createElement("br")); // Add a line break after each checkbox
-//     });
-// });
-
-
-
-
-// function getSelectedValues() {
-//     // Reset selectedValues array
-//     var selectedValues = [];
-
-//     // Get all checkboxes
-//     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-//     // Iterate over each checkbox
-//     checkboxes.forEach(function(checkbox) {
-//         // If checkbox is checked and value is not 'on', add its value to selectedValues array
-//         if (checkbox.checked && checkbox.value !== 'on') {
-//             selectedValues.push(checkbox.value);
-//         }
-//     });
-
-//     var cqlFilterGut = "";
-//     if (selectedValues.length > 0) {
-//         cqlFilterGut = "Gut_No IN (" + selectedValues.map(value => "'" + value + "'").join(",") + ")";
-//     }
-//     var cqlFilter = "";
-//     if (cqlFilterGut && filters) {
-//         cqlFilter = "(" + cqlFilterGut + ") AND (" + filters + ")";
-//     } else {
-//         cqlFilter = cqlFilterGut || filters;
-//     }
-
-//     // Log CQL filter string
-//     console.log(cqlFilter);
-
-//     // Log selected values
-//     console.log(selectedValues);
-
-//     return cqlFilter;
-// }
-
-
-
-// document.getElementById('checkboxContainer').addEventListener('change', function () {
-//     var cqlFilter = getSelectedValues();
-//     // Use cqlFilter as needed, e.g., update the GeoServer layer
-//     console.log(cqlFilter,"fvfdvddshcdc")
-//     FitbouCustomiseRevenue(cqlFilter)
     FitbouCustomiseRevenue(filters)
     Revenue_Layer.setParams({
         CQL_FILTER: filters,
@@ -425,34 +246,6 @@ $("#search_type").change(function () {
 
 })
 
-// draw tool add
-
-// Initialize the FeatureGroup to store drawn items
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-
-// Initialize the Leaflet Draw control
-var drawControl = new L.Control.Draw({
-    draw: {
-      polyline: false,
-        polygon: {
-            shapeOptions: {
-                color: 'red' // Color of the polygon outline
-            },
-            icon: new L.DivIcon({
-            iconSize: new L.Point(4, 4), // reduce the size of the icon
-            className: "leaflet-div-icon", // specify the icon class
-          }),
-        },
-        rectangle: false,
-        circle: false, // Disable drawing circles
-        marker: false // Disable drawing markers
-    },
-    edit: false // Disable edit mode
-});
-map.addControl(drawControl);
-
-// ===============================================
 
 // Create a button element
 var button = L.control({ position: 'topright' });
